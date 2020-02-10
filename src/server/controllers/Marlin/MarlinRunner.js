ce9a102f8378e6d132fb8c5d888c190060289474
Otávio Ribeiro <otavio.ribeiro@gmail.com>
2020-02-09 22:40:47 -0300

    EXTRUDER_COUNT=0

--- src/server/controllers/Marlin/MarlinRunner.js
+++ src/server/controllers/Marlin/MarlinRunner.js
@@ -78,13 +78,7 @@ class MarlinRunner extends events.EventEmitter {
             }
 
             if(extruderCount == 0){
-              const nextState = {
-                  ...this.state,
-                  pos: {
-                      ...this.state.pos,
-                      e: null
-                  }
-              this.state = nextState; // enforce change
+              delete this.state.pos["e"];
             }
 
             this.emit('firmware', payload);
